8f9e8cea0c5d2bb8f3f911c5504335f8fbeed3fe
Hemanth Varma <saripallihemanthvarma1000@gmail.com>
2026-05-28 10:50:37 +0530

    sum func

--- git.c
+++ git.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
+int sum();
 int main()
 {
     printf("hello Hemanth");
     Printf("new branch");
+    printf("%d",sum);
     return 0;
+}
+int sum()
+{
+    int a=5,b=10;
+    return a+b;
 }
